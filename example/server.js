317d159cf9ee6a3331fb6523e6fe2fe652f50cdc
Jake Rigby <jakewigby@gmail.com>
2013-07-03 23:47:06 +0100

    ignoring an assets folder

--- example/server.js
+++ example/server.js
@@ -1,12 +1,11 @@
 var microbe = require('./../lib/microbe/App').App;
-console.log(__dirname+'/public');
 var app = new microbe(__dirname+'/public', 'http://localhost', 1130);
 
 //app.configure(function(user,socket,database){
 
 	app.add(function(api,user){
 
-			api.name = 'increment_score';
+			api.name = 'example_api';
 
 			api.handler = function(apiData, userData, apiUserData, clientData, callback){ // <-- set assumes we persist to the namespace indicated by the api name
 				var gotdbvalue = get;
