e2a6020e6e28a3a4d273bc7fe409193dc8892efd
Jake Rigby <jakewigby@gmail.com>
2013-07-08 18:53:13 +0100

    updated example to include socket api layer

--- example/server.js
+++ example/server.js
@@ -1,5 +1,20 @@
 var microbe = require('./../lib/microbe/microbe').microbe;
 var app = new microbe(__dirname+'/public', 'http://localhost', 1130, 'fbappid', 'fbappsecret');
+var redis = require('redis').createClient();
 
 
-//app.add(require('api.js').api
+app.add(function(socket, user){
+
+		var key = 'microbe-example:'+user.identifier;
+
+		socket.on('add_thing', function(thing){
+			redis.sadd(key, thing);
+		});
+
+		socket.on('get_things', function(){
+			redis.smembers(key, function(err, list){
+				socket.emit('got_things',list);
+			});
+		});
+	}
+);
