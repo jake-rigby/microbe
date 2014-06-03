c814bed35706bb68ca6952a5940b289030888969
Jake Rigby <jakewigby@gmail.com>
2014-06-03 17:47:32 +0100

    Migrating express and socket.io BROKEN

--- modules/init.js
+++ modules/init.js
@@ -20,19 +20,24 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig){
 		http = require('http'),
 		cookie = require('cookie'),
 		redis = require('redis'),
-		socketRedisStore = require('socket.io/lib/stores/redis')		
+		cookieParser = require('cookie-parser'),
+		cookieSignature = require('cookie-signature'),
+		bodyParser = require('body-parser'),
+		socketioRedis = require('socket.io-redis');
+		//socketRedisStore = require('connect-redis/lib/connect-redis')		
+		//socketRedisStore = require('socket.io/lib/stores/redis')		
 
 	
 	// initialise
 	var app = express(),
 		server = http.createServer(app),
 		redisClient = redis.createClient(redisConfig.port,redisConfig.host,{no_ready_check: false}),
-		RedisStore = require('connect-redis')(express),
+		session = require('express-session'),
+		RedisStore = require('connect-redis')(session),
+		sessionStore = new RedisStore({host: 'localhost', port:6379}),
 		io = require('socket.io').listen(server),
 		redisPub = redis.createClient(redisConfig.port,redisConfig.host,{no_ready_check: false}),
-		redisSub = redis.createClient(redisConfig.port,redisConfig.host,{no_ready_check: false}),
-		sessionStore,
-    	cookieParser = require('express/node_modules/cookie');
+		redisSub = redis.createClient(redisConfig.port,redisConfig.host,{no_ready_check: false});
     
     
     // pub and sub..
@@ -110,48 +115,16 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig){
 		})
 	);
 
-
-
 	// configure express
-	app.configure(function(){
-		sessionStore = new RedisStore({client:redisClient});
-		app.use(express.cookieParser('secret')); // <-- #executionorder cookie parser used first
-		app.use(express.bodyParser());
-		app.use(express.session({
-			secret: 'secret', 
-			store : sessionStore ,
-			key : 'express.sid'
-		}));
-		app.use(passport.initialize()); // <-- init passport after session and cookie mware
-	});
+	app.use(bodyParser());
+	app.use(cookieParser('secret')); // <-- #executionorder cookie parser used first
+	app.use(session({
+		secret: 'secret', 
+		store : sessionStore,
+		key : 'express.sid'
+	}));
+	app.use(passport.initialize()); // <-- init passport after session and cookie mware
 
-	// add a route to get the user // 401 is not authorised
-	app.get('/user', function (req, res) {
-		if (req.session.passport && req.session.passport.user && req.session.passport.user != '' )
-			res.send(req.session.passport.user);
-		else
-			res.send(null,401);
-	});
-	
-	// logout	
-	app.get('/logout', function(req,res){
-		req.logout();
-		res.redirect('/');
-	});	
-	
-	// configure passport routes for google login
-	app.get('/auth/google', passport.authenticate('google'));
-	app.get('/auth/google/return', passport.authenticate('google',{successRedirect:'/',failureRedirect:'/'}));
-	
-	
-	// routes for local login
-	app.post('/login', passport.authenticate('local',{successRedirect: '/', failureRedirect: '/'}));
-	
-	
-	// routes for facebook login
-	app.get('/auth/facebook', passport.authenticate('facebook'));
-	app.get('/auth/facebook/callback', 
-		passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login.html' }));
 
 	/*
 	 * passport allows hooks to serialise the user data 
@@ -168,7 +141,8 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig){
 	});
 
 
-	// configure socket.io
+	/* 
+	//configure socket.io
 	io.configure(function () {
 		
 		io.set('log level', 1)
@@ -198,12 +172,62 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig){
 				accept('session error', false)
 			}
 		})
+	
 	});
+	*/
+
+	io.adapter(socketioRedis({
+		host: 'localhost', 
+		port:6379, pubClient: 
+		redisPub
+	}));
+
+	// configure socket.io version 1.0.4
+	io.use(function(socket, next) {
+		
+		var data = socket.request; // <-- the handshakeData
+
+		if (data.headers.cookie) {
+			console.log('your cookie', data.headers.cookie);
+			data.cookie = cookie.parse(data.headers.cookie)
+			//data.cookie = cookieParser.utils.parseSignedCookies(data.cookie, 'secret')
+			//data.cookie = cookieParser.utils.parseJSONCookies(data.cookie)
+			data.sessionID = data.cookie['express.sid'];
+			console.log('your cookie sid', data.sessionID);
+
+			/** The problem seems to be in the signing :
+			 the key in the database is this : Y7fB0f5cLA1MYaLuGVuLTEX6
+			 the store doesnt return a value because the session ID is still signed and looks like this 
+				your cookie io=WwG3K_Rk-gSGa-oJAAAA; express.sid=s%3AncyQX75EZa7wKkPiAsUhYCq1.30yJvcvAFS7wSXsYzFWsUZZQuawSDOHOlcZ1NnFHv24
+
+			is this because I am signing it with cookieParser and unsigning it with cookieSignature?
+			(the line below this returns false)
+
+			**/
+
+			console.log('unsigning',cookieSignature.unsign(data.sessionID, 'secret'));
+			sessionStore.load(data.sessionID, function (err, session) {
+				console.log('stored session',err,session);
+				if (err || !session) {
+					// invalid session identifier. tl;dr gtfo.
+					next(new Error('session error'));
+				} else {
+					data.session = session
+					next();
+				}
+			})
+		} else {
+			console.log('authentication error - no cookie set')
+			next(new Error('authentication error - no cookie set'));
+		}		
+	})
 
 
 	// implement socket.io
 	io.sockets.on('connection',function(socket){
-		socket.session = new connect.middleware.session.Session({ sessionStore: sessionStore }, socket.handshake.session);			
+		//socket.session = new connect.middleware.session.Session({ sessionStore: sessionStore }, socket.handshake.session);
+		socket.session = new session.Session();
+		console.log('connect session', session); // <-- NEVER REACHED
 		if (socket.session && socket.session.passport.user) {
 			//console.log('socket recognising '+socket.session.passport.user.displayName+' in session '+socket.handshake.sessionID);			
 		}
@@ -214,6 +238,7 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig){
 	app.add = function(api) {
 		io.sockets.on('connection', function(socket){
 			socket.session = new connect.middleware.session.Session({ sessionStore: sessionStore }, socket.handshake.session);			
+			console.log('connect session', session); 
 			if (socket.session && socket.session.passport.user) {
 				//console.log('socket recognising '+socket.session.passport.user.displayName+' in session '+socket.handshake.sessionID);			
 				api(socket,socket.session.passport.user);
@@ -221,6 +246,41 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig){
 		});
 	};
 	
+
+	/*
+	 * Routes
+	 */
+
+	// logout	
+	app.get('/logout', function(req,res){
+		req.logout();
+		res.redirect('/');
+	});	
+	
+
+	// configure passport routes for google login
+	app.get('/auth/google', passport.authenticate('google'));
+	app.get('/auth/google/return', passport.authenticate('google',{successRedirect:'/',failureRedirect:'/'}));
+	
+	
+	// routes for local login
+	app.post('/login', passport.authenticate('local',{successRedirect: '/', failureRedirect: '/'}));
+	
+	
+	// routes for facebook login
+	app.get('/auth/facebook', passport.authenticate('facebook'));
+	app.get('/auth/facebook/callback', 
+	passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login.html' }));
+
+
+	// add a route to get the user // 401 is not authorised
+	app.get('/user', function (req, res) {
+		if (req.session.passport && req.session.passport.user && req.session.passport.user != '' )
+			res.send(req.session.passport.user);
+		else
+			res.send(null,401);
+	});
+
 
 	server.listen(port);
 
