987add7213c815e48d86abf830d58c03a3e588f9
Jake Rigby <jakewigby@gmail.com>
2014-09-08 15:50:27 +0100

    added manual session destroy and cookie clear in logout route

--- modules/init.js
+++ modules/init.js
@@ -220,6 +220,8 @@ module.exports = function(callbackurl, port, redisConfig, fbConfig, googleConfig
 	// logout	
 	app.get('/logout', function(req,res){
 		req.logout();
+		req.session.destroy(); // <-- http://stackoverflow.com/questions/13758207/why-is-passportjs-in-node-not-removing-session-on-logout
+		res.clearCookie('express.sid');
 		res.redirect('/');
 	});	
 	
